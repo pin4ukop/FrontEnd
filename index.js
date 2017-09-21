290f031ab216189f1431d855eab4778c421f814e
ksusha <ksusha_email@whatever.com>
2017-09-21 16:10:58 +0300

    dz11-Math.random

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function random() {
     var text = "";
     // var a = prompt("");
     var a = "В каком виде указывать первый параметр"
-    var max = 50;
+    var max = a.length;
     var b = ( Math.random() * max );
     for (var i = 0; i < b; i++) {
         text = text + (a.charAt(Math.floor(Math.random() * a.length)));
